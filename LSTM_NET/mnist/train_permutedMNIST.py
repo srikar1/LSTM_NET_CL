ebfe9bf87c1d02cc6866d376ef0b266b5a0728d7
srikar1 <dupatisrikar12@gmail.com>
2022-10-22 15:16:39 +0530

    Delete LSTM_NET directory

--- LSTM_NET/mnist/train_permutedMNIST.py
+++
@@ -1,7 +0,0 @@
-# Do not delete the following import for all executable scripts!
-import __init__ # pylint: disable=unused-import
-
-from mnist.train_splitMNIST import run
-
-if __name__ == '__main__':
-    run(mode='perm')
